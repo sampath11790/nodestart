391a7d3defcfbe9d30c4d885cae41fbc981fea93
sampath11790 <bsampath11790@gmail.com>
2023-04-15 14:18:03 +0530

    installed express and used its send method and listen

--- myfile.js
+++ myfile.js
@@ -1,11 +1,30 @@
 const http = require("http");
-
+const express = require("express");
+const app = express();
 // custom moudle
-const routes = require("./Router");
-console.log(routes.information);
+// const routes = require("./Router");
+// console.log(routes.information);
 // const textval = require("./form.txt");
-const server = http.createServer(routes.myhandler);
+// const server = http.createServer(routes.myhandler);
+const server = http.createServer(app);
 
-server.listen(3000, () => {
-  console.log("3000 port called");
+//add middleware
+app.use((req, res, next) => {
+  console.log("middleware one");
+  next();
 });
+
+app.use((req, res, next) => {
+  console.log("middleware two");
+  res.send("<h1>hi am express js page </h1>");
+  // res.send({ name: "sam" });
+  // next()
+});
+
+//express creates server for us
+app.listen(3000);
+
+// by create venilla node js method
+// server.listen(3000, () => {
+//   console.log("3000 port called");
+// });
