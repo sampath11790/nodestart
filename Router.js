f7c59aaa33c26b00526fc2b7fbaa8f3884120aa5
sampath11790 <bsampath11790@gmail.com>
2023-04-15 08:13:45 +0530

    split files one large file into  multiple files and exports added

---
+++ Router.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const requestHandler = (req, res) => {
+  console.log("this is create server function ");
+  // console.log(textval);
+
+  if (req.url === "/") {
+    fs.readFile("form.txt", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+
+      res.setHeader("Context-type", "text/html");
+      res.write("<html>");
+      res.write("<head><title>myapplication</title></head>");
+      res.write(`<body><h1>${data}</h1>`);
+      res.write(
+        `<form action=/message method=POST><input type=text name=message><button>send</button></form></body>`
+      );
+
+      res.write("</html>");
+      return res.end();
+    });
+    return;
+    // console.log("2");
+  } else if (req.url === "/home") {
+    res.setHeader("Context-type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>myapplication</title></head>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write(`</html>`);
+    return res.end();
+  } else if (req.url === "/about") {
+    res.setHeader("Context-type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>myapplication</title></head>");
+    res.write("<body><h1>Welcome about page</h1></body>");
+    res.write(`</html>`);
+    return res.end();
+  } else if (req.url === "/message" && req.method === "POST") {
+    var body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      let parseBody = Buffer.concat(body).toString();
+      let messageval = parseBody.split("=")[1];
+      // console.log(messageval);
+      fs.writeFileSync("form.txt", messageval);
+      res.statusCode = 302;
+      // res.write("<h1>some data sending</h1>");
+      res.setHeader("Location", "/");
+
+      return res.end();
+    });
+    // fs.writeFileSync("form.text", "dummy");
+
+    // res.setHeader("Context-type", "text/html");
+    // res.write("<html>");
+    // res.write("<head><title>myapplication</title></head>");
+    // res.write("<body><h1>i from form element thankyou</h1></body>");
+    // res.write(`</html>`);
+    // return res.end();
+  }
+  res.write("<body><h1>page not fount 404</h1></body>");
+  return res.end();
+};
+//if u want remove module its available like global variable window or this
+// method one
+module.exports = {
+  myhandler: requestHandler,
+  information: "this is my route handler function",
+};
+// two
+
+// module.exports.myhandler=requestHandler;
+// module.exports.information= "this is my route handler function";
+
+//three
+
+// exports.myhandler=requestHandler;
+// exports.information= "this is my route handler function";
--- myfile.js
+++ myfile.js
@@ -1,72 +1,10 @@
 const http = require("http");
-const fs = require("fs");
+
+// custom moudle
+const routes = require("./Router");
+console.log(routes.information);
 // const textval = require("./form.txt");
-const server = http.createServer((req, res) => {
-  console.log("this is create server function ");
-  // console.log(textval);
-
-  if (req.url === "/") {
-    fs.readFile("form.txt", "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-
-      res.setHeader("Context-type", "text/html");
-      res.write("<html>");
-      res.write("<head><title>myapplication</title></head>");
-      res.write(`<body><h1>${data}</h1>`);
-      res.write(
-        `<form action=/message method=POST><input type=text name=message><button>send</button></form></body>`
-      );
-
-      res.write("</html>");
-      return res.end();
-    });
-    return;
-    // console.log("2");
-  } else if (req.url === "/home") {
-    res.setHeader("Context-type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>myapplication</title></head>");
-    res.write("<body><h1>Welcome home</h1></body>");
-    res.write(`</html>`);
-    return res.end();
-  } else if (req.url === "/about") {
-    res.setHeader("Context-type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>myapplication</title></head>");
-    res.write("<body><h1>Welcome about page</h1></body>");
-    res.write(`</html>`);
-    return res.end();
-  } else if (req.url === "/message" && req.method === "POST") {
-    var body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      let parseBody = Buffer.concat(body).toString();
-      let messageval = parseBody.split("=")[1];
-      // console.log(messageval);
-      fs.writeFileSync("form.txt", messageval);
-      res.statusCode = 302;
-      // res.write("<h1>some data sending</h1>");
-      res.setHeader("Location", "/");
-
-      return res.end();
-    });
-    // fs.writeFileSync("form.text", "dummy");
-
-    // res.setHeader("Context-type", "text/html");
-    // res.write("<html>");
-    // res.write("<head><title>myapplication</title></head>");
-    // res.write("<body><h1>i from form element thankyou</h1></body>");
-    // res.write(`</html>`);
-    // return res.end();
-  }
-  res.write("<body><h1>page not fount 404</h1></body>");
-  return res.end();
-});
+const server = http.createServer(routes.myhandler);
 
 server.listen(3000, () => {
   console.log("3000 port called");
